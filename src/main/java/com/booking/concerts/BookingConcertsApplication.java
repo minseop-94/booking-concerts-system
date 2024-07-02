9e542194fe6f18e6e6112f969029dd4c87a77349
b_loso <jaesme2530@gmail.com>
2024-07-03 00:02:39 +0900

    [INIT] 스프링 부트 프로젝트 초기화

---
+++ src/main/java/com/booking/concerts/BookingConcertsApplication.java
@@ -0,0 +1,13 @@
+package com.booking.concerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingConcertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingConcertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booking/concerts/BookingConcertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.concerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingConcertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
